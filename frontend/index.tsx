5fa6a35404d4f21c71381520d719d02a96c858d3
Mateusz Pałka <mateuszpalka90@gmail.com>
2024-02-17 22:28:18 +0100

    fix issues

--- frontend/index.tsx
+++ frontend/index.tsx
@@ -8,12 +8,12 @@ import React from "react";
 import { Selector } from "./selector";
 import { Record } from "./components/record";
 import { Info } from "./components/info";
-import { FieldId } from "./types";
+import { FieldId, TableId } from "./types";
 
 function App() {
   const base = useBase();
   const globalConfig = useGlobalConfig();
-  const tableId = globalConfig.get("selectedTableId") as string | null;
+  const tableId = globalConfig.get(TableId.Main) as string | null;
 
   const table = base.getTableByIdIfExists(tableId);
 
@@ -25,12 +25,20 @@ function App() {
   const currencyFieldId = globalConfig.get(FieldId.Currency) as string | null;
   const resultFieldId = globalConfig.get(FieldId.Result) as string | null;
 
-  const { hasPermission } = table.checkPermissionsForUpdateRecord();
+  const { hasPermission } = table?.checkPermissionsForUpdateRecord() || {
+    hasPermission: false,
+  };
+
+  const hasAllFieldsSelected =
+    !!table &&
+    !!exchangeDateFieldId &&
+    !!valueFieldId &&
+    !!currencyFieldId &&
+    !!resultFieldId;
 
   return (
     <main style={{ padding: "1rem", minWidth: "400px" }}>
       <Selector />
-
       <div
         style={{
           height: "1px",
@@ -41,65 +49,69 @@ function App() {
       />
       <Info />
       {hasPermission ? (
-        <section>
-          <table
-            style={{
-              width: "100%",
-            }}
-          >
-            <thead>
-              <tr>
-                <th style={{ borderBottom: "1px solid black" }}>Name</th>
-                <th style={{ borderBottom: "1px solid black" }}>Date</th>
-                <th style={{ borderBottom: "1px solid black" }}>Value</th>
-                <th style={{ borderBottom: "1px solid black" }}>Currency</th>
-                <th style={{ borderBottom: "1px solid black" }}>
-                  Exchanged to
-                </th>
-              </tr>
-            </thead>
+        hasAllFieldsSelected ? (
+          <section>
+            <table
+              style={{
+                width: "100%",
+              }}
+            >
+              <thead>
+                <tr>
+                  <th style={{ borderBottom: "1px solid black" }}>Name</th>
+                  <th style={{ borderBottom: "1px solid black" }}>Date</th>
+                  <th style={{ borderBottom: "1px solid black" }}>Value</th>
+                  <th style={{ borderBottom: "1px solid black" }}>Currency</th>
+                  <th style={{ borderBottom: "1px solid black" }}>
+                    Exchanged to
+                  </th>
+                </tr>
+              </thead>
 
-            <tbody>
-              {records?.map((record, i) => {
-                const exchangeDate = record.getCellValue(
-                  exchangeDateFieldId
-                ) as string | null;
-                const value = record.getCellValue(valueFieldId) as
-                  | number
-                  | null;
-                const currency = record.getCellValue(currencyFieldId) as
-                  | {
-                      color: string;
-                      id: string;
-                      name: string;
-                    }
-                  | string
-                  | null;
-                const result = record.getCellValue(resultFieldId) as
-                  | number
-                  | null;
-                return (
-                  <Record
-                    key={record.id}
-                    record={record}
-                    exchangeDate={exchangeDate}
-                    value={value}
-                    currency={currency}
-                    result={result}
-                    darkBg={!!(i % 2)}
-                  />
-                );
-              })}
-            </tbody>
-          </table>
-        </section>
+              <tbody>
+                {records?.map((record, i) => {
+                  const exchangeDate = record.getCellValue(
+                    exchangeDateFieldId
+                  ) as string | null;
+                  const value = record.getCellValue(valueFieldId) as
+                    | number
+                    | null;
+                  const currency = record.getCellValue(currencyFieldId) as
+                    | {
+                        color: string;
+                        id: string;
+                        name: string;
+                      }
+                    | string
+                    | null;
+                  const result = record.getCellValue(resultFieldId) as
+                    | number
+                    | null;
+                  return (
+                    <Record
+                      key={record.id}
+                      record={record}
+                      exchangeDate={exchangeDate}
+                      value={value}
+                      currency={currency}
+                      result={result}
+                      darkBg={!!(i % 2)}
+                    />
+                  );
+                })}
+              </tbody>
+            </table>
+          </section>
+        ) : null
       ) : (
-        <section style={{ backgroundColor: "red", padding: "0.25rem 1rem" }}>
-          <h3 style={{ color: "white" }}>
-            You do not have permission to update records. Contact table
-            administrator to update your permission
-          </h3>
-        </section>
+        table && (
+          <section style={{ backgroundColor: "red", padding: "0.25rem 1rem" }}>
+            <h3 style={{ color: "white" }}>
+              You do not have permission to update records. Contact table
+              administrator to update your permission
+            </h3>
+          </section>
+        )
       )}
     </main>
   );
